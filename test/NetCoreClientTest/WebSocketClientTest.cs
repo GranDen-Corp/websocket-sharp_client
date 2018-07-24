c5af80c195121e22c205700bd90341eff9ade136
Chen Yu Pao <windperson@gmail.com>
2018-07-24 17:05:56 +0800

    Add more test

--- test/NetCoreClientTest/WebSocketClientTest.cs
+++ test/NetCoreClientTest/WebSocketClientTest.cs
@@ -220,5 +220,61 @@ namespace NetCoreClientTest
                 }
             }
         }
+
+        [Fact]
+        public async Task ClientCanSendWhatTextItReceived()
+        {
+            //Arrange
+            const string clientAddress = "ws://localhost:54325/ws";
+            const string serverAddress = "http://localhost:54325";
+
+            var config = NetCoreWebSocketHelper.CreateConfigWithUrl(serverAddress);
+            const string textData = "Hello World";
+            var serverAction = new Func<HttpContext, Task>(async httpContext =>
+            {
+                if (httpContext.Request.Path == "/ws")
+                {
+                    Assert.True(httpContext.WebSockets.IsWebSocketRequest);
+                    var webSocket = await httpContext.WebSockets.AcceptWebSocketAsync();
+
+                    var serverBuffer = new byte[1024 * 4];
+
+                    await webSocket.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes(textData)),
+                        WebSocketMessageType.Text, true, CancellationToken.None);
+
+                    var receiveResult =
+                        await webSocket.ReceiveAsync(new ArraySegment<byte>(serverBuffer), CancellationToken.None);
+                    Assert.True(receiveResult.EndOfMessage);
+                    Assert.Equal(WebSocketMessageType.Text, receiveResult.MessageType);
+                    Assert.Equal(textData, DataHelper.GetReadableString(serverBuffer));
+
+                    while (!receiveResult.CloseStatus.HasValue)
+                    {
+                        receiveResult = await webSocket.ReceiveAsync(new ArraySegment<byte>(serverBuffer),
+                            CancellationToken.None);
+                    }
+
+                    await webSocket.CloseAsync(receiveResult.CloseStatus.Value, receiveResult.CloseStatusDescription,
+                        CancellationToken.None);
+                }
+            });
+            using (var server = NetCoreWebSocketHelper.CreateTestServer(config, _testOutputHelper, serverAction))
+            {
+                await server.StartAsync();
+                using (var ws = new WebSocketSharp.WebSocket(clientAddress))
+                {
+                    var hasSent = false;
+                    ws.OnMessage += (sender, args) =>
+                    {
+                        var data = args.Data;
+                        ws.Send(data);
+                        hasSent = true;
+                    };
+                    ws.Connect();
+                    SpinWait.SpinUntil(() => hasSent);
+                    ws.Close();
+                }
+            }
+        }
     }
 }
